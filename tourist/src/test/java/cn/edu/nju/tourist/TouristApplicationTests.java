347fe94437daa9758085ca766b60c713b4efb0c1
meow <fxsmeowmeow@gmail.com>
2019-01-13 14:22:40 +0800

    add a new project

---
+++ tourist/src/test/java/cn/edu/nju/tourist/TouristApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.edu.nju.tourist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TouristApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
